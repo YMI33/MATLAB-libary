fa027e0845c334b151891e7318a29d7d57e7346e
Zhiang Xie <xiezhiang@lasg.iap.ac.cn>
2017-12-08 15:24:42 +0800

    add print setting

--- drawpic.m
+++ drawpic.m
@@ -1,22 +1,43 @@
 % draw Picture
 
 %shaded
-ytick=[-.2:.04:.2];
-[c,h]=contourf(X(arealev,arealat),Y(arealev,arealat),wca_omega(:,:,k)');  %后面的是设置等值线的颜色和宽度
+ytick=[-.2:.04:.2];%等值线数值
+[c,h]=contourf(X,Y,wca_omega(:,:,k)');  %后面的是设置等值线的颜色和宽度
 set(h,'LevelList',ytick); %设置等值线间距
 shading flat
 
-hold on
 %contour
-[c,h]=contour(X(arealev,arealat),Y(arealev,arealat),wca_thetae(:,:,k)','Linecolor','k','LineWidth',2);  %后面的是设置等值线的颜色和宽度
+[c,h]=contour(X,Y,wca_thetae(:,:,k)','Linecolor','k','LineWidth',2);  %后面的是设置等值线的颜色和宽度
 text_handle=clabel(c,h,'Fontsize',12);   %加入等值线数值 
 set(h,'LevelList',[335:5:360],'TextList',[335:5:360]); %设置等值线间距
 
-hold off
-set(gca,'LineWidth',2,'FontSize',12,'Ylim',[lev(arealev(end)) lev(arealev(1))],'Xlim',[lat(arealat(1)) lat(arealat(end))],'Clim',[ytick(1) ytick(end)]...  
-    ,'XTick',lat(arealat(1)):5:lat(arealat(end)),'YDir','reverse'); %添加坐标经纬度坐标轴
-colorbar('YTick',ytick)
-title('/////','Fontsize',16)
+%--------------------------------------axis seting------------------------------------------
+%++++++++++++++++++++++++++++++basic longitude&latitude setting
+%horizonal
+xtick=60:10:150;
+ytick=10:5:45;
+for k=1:length(xtick)
+  ylabel{k}=[xtick(k) 'N'];
+end
+for k=1:length(ytick)
+  xlabel{k}=[ytick(k) 'E'];
+end
+set(gca,'Fontsize',16,'LineWidth',2,'xlim',[xtick(1) xtick(end)],'ylim',[ytick(1) ytick(end)],...
+    'Xtick',xtick,'Ytick',ytick,'XTickLabel',xlabel,'YTickLabel',ylabel,...
+    'XGrid','on','YGrid','on','ZGrid','on')
+%vertical    
+ztick=[200 300 400 500 700 850];
+for k=1:length(ztick)
+  zlabel{k}=[num2str(ztick(k)) 'hPa'];
+end
+set(gca,,'zlim',[ztick(1) ztick(end)],'Ztick',ztick,'ZTickLabel',zlabel,)
 
+%+++++++++++++++++++++++++++++colormap
+set(gca,'clim',[ctick(1) ctick(end)])
+colorbar('YTick',ctick)
 
+%+++++++++++++++++++++++++++print setting
+set(gcf,'Paperunit','centimeters','PaperPosition',[5 5 20 15]);
+print('-dtiff','-r300','.\3Dstructure.tiff')
+%format: %03d为001
 
